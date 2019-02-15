e1a248bd2f5305920fc0dc189677d64b03bebb51
believer44 <believer44@163.com>
2019-02-16 03:36:11 +0800

    1.0

--- http.go
+++ http.go
@@ -1,72 +1,86 @@
-package main
+package httpSimple
 
 import (
-	"HttpRequest"
-	"fmt"
+	"bytes"
+	//	"fmt"
+	"io/ioutil"
+
+	//	"fmt"
+	"compress/gzip"
 	"log"
+	"time"
+
+	"github.com/kirinlabs/HttpRequest"
 )
 
-func main() {
-
+func SGet(url string, query map[string]interface{}, headers, cookies map[string]string, timeout int) ([]byte, error) {
+	//var resp *HttpRequest.Response
 	req := HttpRequest.NewRequest()
-
 	// 设置超时时间，不设置时，默认30s
-	req.SetTimeout(5)
-
+	req.SetTimeout(time.Duration(timeout) * time.Second)
 	// 设置Headers
-	req.SetHeaders(map[string]string{
-		"Content-Type": "application/x-www-form-urlencoded", //这也是HttpRequest包的默认设置
-	})
-
+	req.SetHeaders(headers)
 	// 设置Cookies
-	req.SetCookies(map[string]string{
-		"sessionid": "LSIE89SFLKGHHASLC9EETFBVNOPOXNM",
-	})
-
-	postData := map[string]interface{}{
-		"id":    1,
-		"title": "csdn",
-	}
-
+	req.SetCookies(cookies)
 	// GET 默认调用方法
-	resp, err := req.Get("http://127.0.0.1:8000?name=flyfreely", nil)
-
+	resp, err := req.Get(url, nil)
 	// GET 传参调用方法
 	// 第2个参数默认为nil，也可以传参map[string]interface{}
 	// 第2个参数不为nil时，会把传入的map以query传参的形式重新构造新url
 	// 新的URL: http://127.0.0.1:8000?name=flyfreely&id=1&title=csdn
-
-	//resp, err := req.Get("http://127.0.0.1:8000?name=flyfreely", postData)
-
-	// POST 调用方法
-
-	//resp, err := req.Post("http://127.0.0.1:8000", postData)
-
 	if err != nil {
 		log.Println(err)
-		return
+		return []byte(""), err
 	}
-
 	if resp.StatusCode() == 200 {
 		body, err := resp.Body()
-
 		if err != nil {
 			log.Println(err)
-			return
+			return []byte(""), err
 		}
+		//fmt.Println(resp.Response().Header.Get("Content-Encoding"))
+		if resp.Response().Header.Get("Content-Encoding") == "gzip" {
+			//var unbody string
+			r, _ := gzip.NewReader(bytes.NewReader(body))
+			defer r.Close()
+			return ioutil.ReadAll(r)
 
-		fmt.Println(string(body))
+		}
+		return body, nil
+	} else {
+		return []byte(""), nil
 	}
-
-	//或者打印Json
+}
+func SPost(url string, data map[string]interface{}, query map[string]interface{}, headers, cookies map[string]string, timeout int) ([]byte, error) {
+	req := HttpRequest.NewRequest()
+	// 设置超时时间，不设置时，默认30s
+	req.SetTimeout(time.Duration(timeout) * time.Second)
+	// 设置Headers
+	req.SetHeaders(headers)
+	// 设置Cookies
+	req.SetCookies(cookies)
+	// Post 默认调用方法
+	resp, err := req.Post(url, data)
+	if err != nil {
+		log.Println(err)
+		return []byte(""), err
+	}
 	if resp.StatusCode() == 200 {
-		body, err := resp.Json()
-
+		body, err := resp.Body()
 		if err != nil {
 			log.Println(err)
-			return
+			return []byte(""), err
 		}
+		if resp.Response().Header.Get("Content-Encoding") == "gzip" {
+			//var unbody string
+			r, _ := gzip.NewReader(bytes.NewReader(body))
+			defer r.Close()
+			return ioutil.ReadAll(r)
 
-		fmt.Println(body)
+		}
+		return body, nil
+	} else {
+		return []byte(""), nil
 	}
+
 }
